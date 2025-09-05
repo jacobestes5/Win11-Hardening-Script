f3ba9ae884742e685240dd4530bfc038c0d0d5dd
Jacob Estes <jacob.estes@student.central.edu>
2025-09-05 10:51:34 -0500

    asdf

---
+++ harden.ps1
@@ -0,0 +1,7 @@
+Write-Host "Script Run Time: $(Get-Date)"
+# Display the computer's hostname
+Write-Host "Computer Name: $env:COMPUTERNAME"
+
+# Display the Windows version
+Write-Host "Windows Version:"
+Get-ComputerInfo | Select-Object -Property WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer
